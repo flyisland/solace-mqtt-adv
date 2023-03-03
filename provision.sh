4926c31d32af64a9b1ef95cfc609913c6daf8a47
flyisland <flyisland@gmail.com>
2023-03-03 11:14:33 +0800

    extract func provision()

--- provision.sh
+++ provision.sh
@@ -5,31 +5,14 @@ log() {
    echo "$(date +'%Y-%m-%d %H:%M:%S'): $1"
 }
 
-create_queue_template() {
-  local configUrl="${solace_admin_url}/SEMP/v2/config/msgVpns/default/queueTemplates"
-  local response=$(curl -X POST --write-out '%{http_code}' --silent --output /dev/null -u ${USER} ${configUrl} -H 'content-type: application/json' -d '{"queueTemplateName":"mqtt_template","maxTtl":600,"respectTtlEnabled":true}')
+provision(){
+  local goal=$1
+  local configUrl=$solace_admin_url/SEMP/v2/config$2
+  local action=$3
+  local jsonBody=$4
+  local response=$(echo "$jsonBody" | curl -X $action --write-out '%{http_code}' --silent --output /dev/null -u $USER $configUrl -H 'content-type: application/json' -d @-)
   if [ "$response" == "200" ] ; then
-    log "Create Queue Template mqtt_template successfully"
-  else
-    log "${configUrl} -> ${response}"
-  fi
-}
-
-update_client_profile() {
-  local configUrl="${solace_admin_url}/SEMP/v2/config/msgVpns/default/clientProfiles/default"
-  local response=$(curl -X PATCH --write-out '%{http_code}' --silent --output /dev/null -u ${USER} ${configUrl} -H 'content-type: application/json' -d '{"apiQueueManagementCopyFromOnCreateTemplateName": "mqtt_template"}')
-  if [ "$response" == "200" ] ; then
-    log "Update Client Profile default successfully"
-  else
-    log "${configUrl} -> ${response}"
-  fi
-}
-
-enable_replay() {
-  local configUrl="${solace_admin_url}/SEMP/v2/config/msgVpns/default/replayLogs"
-  local response=$(curl -X POST --write-out '%{http_code}' --silent --output /dev/null -u ${USER} ${configUrl} -H 'content-type: application/json' -d '{"egressEnabled":true,"ingressEnabled":true,"maxSpoolUsage":1000,"replayLogName":"defaultLog"}')
-  if [ "$response" == "200" ] ; then
-    log "Enable Replay successfully"
+    log "$goal successfully"
   else
     log "${configUrl} -> ${response}"
   fi
@@ -40,6 +23,17 @@ if [ -z ${solace_admin_url+x} ]; then
   solace_admin_url="http://localhost:8080"
 fi
 
-create_queue_template
-update_client_profile
-enable_replay
+provision "Create Queue Template mqtt_template" \
+  "/msgVpns/default/queueTemplates" \
+  "POST" \
+  '{"queueTemplateName":"mqtt_template","maxTtl":600,"respectTtlEnabled":true}'
+
+provision "Update Client Profile default" \
+  "/msgVpns/default/clientProfiles/default" \
+  "PATCH" \
+  '{"apiQueueManagementCopyFromOnCreateTemplateName":"mqtt_template"}'
+
+provision "Enable Replay" \
+  "/msgVpns/default/replayLogs" \
+  "POST" \
+  '{"egressEnabled":true,"ingressEnabled":true,"maxSpoolUsage":1000,"replayLogName":"defaultLog"}'
